711a50ecd21807b252c8d00bd0299a58102ccbc5
DavidFinotto <60079161+DavidFintt@users.noreply.github.com>
2023-08-17 21:57:21 -0300

    Alter function based views for viewsets

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from vehicles.views import *
+from rest_framework.routers import SimpleRouter
+
+registerVehicleRouter = SimpleRouter()
+registerVehicleRouter.register(
+    'vehicleList/api/v1',
+    vehicleList
+)
 
 urlpatterns = [
-    path('vehicleList/api/v1/',
-         lista_veiculos,         name="lista_veiculos"),
-    path('vehicleList/api/v1/<int:id>',
-         detalhes_veiculo,       name="detalhes_veiculo")
+#     path('vehicleList/api/v1/',
+#          lista_veiculos,         name="lista_veiculos"),
+#     path('vehicleList/api/v1/<int:id>',
+#          detalhes_veiculo,       name="detalhes_veiculo")
+
+     path('', include(registerVehicleRouter.urls))
 ]
+
