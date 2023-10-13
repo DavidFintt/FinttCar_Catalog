fec2b8e2045d4dc84e07f0987c47f3d993b079c8
DavidFinotto <60079161+DavidFintt@users.noreply.github.com>
2023-10-12 22:08:23 -0300

    Add images

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from string import ascii_letters
+import random
+import os
 
 # Create your models here.
 
+def renameImage(file):
+    new_name = ''
+    for i in range(7):
+        new_name += random.choice(ascii_letters) + str(random.choice(range(1,10)))
+    extension = os.path.splitext(file)[1]
+
+    full_name = new_name + extension
+
+    return full_name
 
 class Users(AbstractUser):
     dealership = models.ForeignKey('Dealership', on_delete=models.CASCADE, related_name="user_dealership", null=True)
@@ -22,6 +34,7 @@ class Manufacturer(models.Model):
         return self.name
 
 class Vehicles(models.Model):
+    img = models.ImageField(upload_to=renameImage('./media_root/images/'))
     highlights = models.BooleanField(default=False)
     new = models.BooleanField(default=True)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
@@ -34,3 +47,5 @@ class Vehicles(models.Model):
 
     def __str__(self) -> str:
         return self.model
+    
+
--- vehicles/serializers.py
+++ vehicles/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 from .models import Manufacturer, Vehicles
 from .validators import VehiclesValidators
+from django.core.files.storage import default_storage
+from os.path import abspath
 
 
 class vehiclesSerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Vehicles
-        fields = ['id', 'year', 'model', 'capacity', 'price', 'manufacturer', 'dealership','amount']
+        fields = ['id', 'year', 'model', 'capacity', 'price', 'manufacturer', 'dealership','amount', 'highlights', 'img']
 
     manufacturer = serializers.StringRelatedField()
     dealership = serializers.StringRelatedField()
+    
 
     def validate(self, attrs):
         VehiclesValidators(data=attrs, ErrorClass=serializers.ValidationError)
         return super().validate(attrs)
+
+    # def get_img(self, obj):
+    #     request = self.context.get('request')
+    #     if obj.img:
+    #         img_path = abspath(obj.img.path)
+    #         return request.build_absolute_uri(img_path)
+    #     return None
